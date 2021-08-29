5d8bfdad0c9ae58d773bf1207a0b409d53042283
Leandro Berne <leandro@plw.com.ar>
2021-08-29 18:46:04 -0300

    Entrega desafio 9

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
 import './ItemCount.css';
 
-const ItemCount = ({ stock, initial = 1 }) => {
+const ItemCount = ({ stock, initial = 1, onAdd }) => {
+  const [number, setNumber] = useState(initial);
 
-    const [number, setNumber] = useState(initial);
-
-    const onAdd = () => {
-        if (number < stock) {
-            setNumber(number + 1);
-        }
+  const onIncrease = () => {
+    if (number < stock) {
+      setNumber(number + 1);
     }
+  };
 
-    const onRemove = () => {
-        if (number > initial) {
-            setNumber(number - 1);
-        }
+  const onDecrease = () => {
+    if (number > initial) {
+      setNumber(number - 1);
     }
+  };
 
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">Cantidad: {number}</h5>
-                <button href="/#" className="btn btn-primary" onClick={onRemove}>-</button>
-                <button href="/#" className="btn btn-success" onClick={onAdd}>+</button>
-                <button href="/#" className="btn btn-secondary">Agregar al carrito</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='card'>
+      <div className='card-body'>
+        <h5 className='card-title'>Cantidad: {number}</h5>
+        <button className='btn btn-primary' onClick={onDecrease}>
+          -
+        </button>
+        <button className='btn btn-success' onClick={onIncrease}>
+          +
+        </button>
+        <button className='btn btn-secondary' onClick={() => onAdd(number)}>
+          Agregar al carrito
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ItemCount;
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ItemDetail.css';
 import ItemCount from '../ItemCount/ItemCount';
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({ data }) => {
-    return (
-        <>
-            <div className="ItemDetail">
-                <div className="ItemDetail-image">
-                    <img src={data.image} alt="Imagen de Producto" />
-                </div>
-                <div className="ItemDetail-content">
-                    <h2>{data.title}</h2>
-                    <h3>$ {data.price}</h3>
-                    <p>{data.description}</p>
-                    <ItemCount stock={10} initial={1} />
-                </div>
-            </div>
-        </>
-    );
-}
+  const [value, setValue] = useState('');
+
+  const onAdd = (quantity) => {
+    setValue(quantity);
+  };
+
+  return (
+    <>
+      <div className='ItemDetail'>
+        <div className='ItemDetail-image'>
+          <img src={data.image} alt='Imagen de Producto' />
+        </div>
+        <div className='ItemDetail-content'>
+          <h2>{data.title}</h2>
+          <h3>$ {data.price}</h3>
+          <p>{data.description}</p>
+          {value ? (
+            <Link to='/cart'>
+              <button className='btn btn-primary'>Terminar compra</button>
+            </Link>
+          ) : (
+            <ItemCount stock={10} initial={1} onAdd={onAdd} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ItemDetail;
