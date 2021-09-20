3a9156a23b6796640808476152e937e3dc0460aa
Leandro Berne <leandro@plw.com.ar>
2021-09-20 07:07:09 -0300

    Agregada funcionalidad Payment

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,15 +1,100 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './Payment.css';
 import { CartContext } from '../../CartContext';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { db } from '../../firebase';
 
 const Payment = () => {
   const { cart, clear, totalPrice } = useContext(CartContext);
 
+  const [client, setClient] = useState({
+    name: '',
+    phone: '',
+    email: '',
+  });
+
+  const [purchaseID, setPurchaseID] = useState('');
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setClient({
+      ...client,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const addPurchase = async (e) => {
+    const purchases = db.collection('purchases');
+    const order = {
+      buyer: {
+        name: client.name,
+        phone: client.phone,
+        email: client.email,
+      },
+      items: cart,
+      date: firebase.firestore.Timestamp.fromDate(new Date()),
+      total: totalPrice(cart),
+    };
+
+    e.preventDefault();
+
+    await purchases
+      .add(order)
+      .then((docRef) => {
+        setPurchaseID(docRef.id);
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+
+    clear();
+    e.target.reset();
+  };
+
   return (
-    <div>
+    <>
       <h1>Payment (garpa loco)</h1>
       <p>Total a garpar: $ {totalPrice(cart)}</p>
-    </div>
+      <form onSubmit={addPurchase}>
+        <div>
+          <label htmlFor='name'>Nombre: </label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Ingrese Nombre'
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='phone'>Telefono: </label>
+          <input
+            type='text'
+            name='phone'
+            id='phone'
+            placeholder='Ingrese Telefono'
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email: </label>
+          <input
+            type='text'
+            name='email'
+            id='email'
+            placeholder='Ingrese Email'
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type='submit'>PAGAR</button>
+      </form>
+      {purchaseID && (
+        <h3>
+          Su numero ID de compra es: <strong>{purchaseID}</strong>
+        </h3>
+      )}
+    </>
   );
 };
 
